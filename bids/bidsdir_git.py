d99b48b3b5a7899ae4e5ff6ab04b4f1ab38bb62d
Andre Oliveira Beukers <andre.olibeu@gmail.com>
2016-07-12 20:04:18 -0400

    bids dir

--- bids/bidsdir_git.py
+++ bids/bidsdir_git.py
@@ -6,6 +6,16 @@ Based off of mvpa2.OpenFMRIDataset
 import os
 from os.path import join as _opj
 
+def _parse_filename(fname):
+    """ By Hanke
+    """
+    components = fname.split('_')
+    ftype = components[-1]
+    components = dict([c.split('-') for c in components[:-1]])
+    components['filetype'] = ftype
+    return components
+
+
 class bidsdir(object):
 
     def __init__(self, basedir):
@@ -23,45 +33,49 @@ class bidsdir(object):
         return subj_ids
 
 
-    def get_bold_run_filenames(self, subj, task, ses=None):
-        if ses == None: ses = task
+    def get_bold_run_filenames(self, subj, ses=''):
+        
         sub=self.get_subj_foldernames()[subj-1]
-        ses='ses-'+ses
-        task='task-'+task
+        if len(ses)>0: ses = 'ses-' + ses   
+
         bolds=[]
-        for fil in os.listdir(_opj(self.basedir,sub,ses,'func')):
+        for fil in os.listdir(_opj(self.basedir,sub,ses,'func')):            
             if fil.endswith('bold.nii') or fil.endswith('bold.nii.gz'):
                 bolds.append(fil)
-        return bolds        
+        return bolds
+
+    def get_tasks(self, subj, ses=''):
+        tasks=[]
+        for run_filename in self.get_bold_run_filenames(subj,ses):
+            tsk=_parse_filename(run_filename)['task']
+            tasks.append(tsk)
+        return tasks
 
 
-    def get_task_bold_run_filenames(self, task, ses=None):
+    def get_task_bold_run_filenames(self, task='', ses=''):
         """
         """
-        if ses == None: ses = task
-
+        
         out = {}
         for sub in range(len(self.get_subj_foldernames())):
-            runs = self.get_bold_run_filenames(sub, ses, task)
+            runs = self.get_bold_run_filenames(sub, ses)
             if len(runs):
                 out[sub+1] = runs
         return out
 
-        def _parse_filename(fname):
-            """ By Hanke
-            """
-            components = fname.split('_')
-            ftype = components[-1]
-            components = dict([c.split('-') for c in components[:-1]])
-            components['filetype'] = ftype
-            return components
+
  
         
 
-basedir = '/Users/andrebeukers/Documents/fMRI/Python/Study Forrest/video_studyforrest'
+
+""" For testing """
+
+basedir = '/Users/andrebeukers/Documents/fMRI/Python/dataset/raw_data'
 bids = bidsdir(basedir)
-bids.get_subj_foldernames
-bids.get_bold_run_filenames(1,task='movie')
-bids.get_task_bold_run_filenames(task='movie')
+# bids.get_subj_foldernames()
+# bids.get_bold_run_filenames(subj=3)
+bids.get_tasks(subj=3)
+# bids.get_task_bold_run_filenames()
 
-
+# sample_fname = bids.get_bold_run_filenames(1,'movie')[0]
+# _parse_filename(sample_fname)
