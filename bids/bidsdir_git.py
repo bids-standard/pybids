07e66601cbf9732b0d20933b34bf6a2586aace33
Andre Oliveira Beukers <andre.olibeu@gmail.com>
2016-07-12 21:25:40 -0400

    _assemble_bold_path and get_subj_bold_paths

--- bids/bidsdir_git.py
+++ bids/bidsdir_git.py
@@ -15,6 +15,12 @@ def _parse_filename(fname):
     components['filetype'] = ftype
     return components
 
+def _assemble_bold_path(fname):
+    """sub-04_ses-movie_task-movie_run-7_bold.nii.gz"""
+    file_info = _parse_filename(fname)
+    return _opj('sub-' + file_info['sub'], 'ses-' + file_info['ses'], 'func')
+
+
 
 class bidsdir(object):
 
@@ -33,7 +39,7 @@ class bidsdir(object):
         return subj_ids
 
 
-    def get_bold_run_filenames(self, subj, ses=''):
+    def get_subj_bold_filenames(self, subj, ses=''):
         
         sub=self.get_subj_foldernames()[subj-1]
         if len(ses)>0: ses = 'ses-' + ses   
@@ -46,23 +52,33 @@ class bidsdir(object):
 
     def get_tasks(self, subj, ses=''):
         tasks=[]
-        for run_filename in self.get_bold_run_filenames(subj,ses):
+        for run_filename in self.get_subj_bold_filenames(subj,ses):
             tsk=_parse_filename(run_filename)['task']
             tasks.append(tsk)
         return tasks
 
 
-    def get_task_bold_run_filenames(self, task='', ses=''):
+    def get_task_bold_run_filenames(self, ses='', task=''):
         """
         """
         
         out = {}
         for sub in range(len(self.get_subj_foldernames())):
-            runs = self.get_bold_run_filenames(sub, ses)
+            runs = self.get_subj_bold_filenames(sub, ses)
             if len(runs):
                 out[sub+1] = runs
         return out
 
+    def get_subj_bold_paths(self, subj, ses='', task=''):
+
+        sample_file = self.get_subj_bold_filenames(subj, ses)[0]
+        path=_opj(self.basedir, _assemble_bold_filepath(sample_file)) 
+
+        bold = []
+        for fil in os.listdir(path):
+            if fil.endswith(('_bold.nii','_bold.nii.gz')):
+                bold.append(_opj(path,fil))
+        return bold
 
  
         
@@ -70,12 +86,14 @@ class bidsdir(object):
 
 """ For testing """
 
-basedir = '/Users/andrebeukers/Documents/fMRI/Python/dataset/raw_data'
+#basedir = '/Users/andrebeukers/Documents/fMRI/Python/dataset/raw_data'
+basedir = '/Users/andrebeukers/Documents/fMRI/Python/Study Forrest/video_studyforrest'
 bids = bidsdir(basedir)
 # bids.get_subj_foldernames()
-# bids.get_bold_run_filenames(subj=3)
-bids.get_tasks(subj=3)
-# bids.get_task_bold_run_filenames()
+# bids.get_subj_bold_filenames(subj=3)
+#bids.get_tasks(subj=3)
+#bids.get_task_bold_run_filenames(ses='movie')
+bids.get_subj_bold_paths(subj=2,ses='movie')
 
-# sample_fname = bids.get_bold_run_filenames(1,'movie')[0]
+# sample_fname = bids.get_subj_bold_filenames(1,'movie')[0]
 # _parse_filename(sample_fname)
