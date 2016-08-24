e8bbefc448a0f2653ba22b3f57e380b654c778dc
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-08-23 18:20:36 -0700

    updated schema and added get_metadata()

--- bids/grabbids/bids_layout.py
+++ bids/grabbids/bids_layout.py
@@ -1,8 +1,42 @@
 from grabbit import Layout
 import os
 import re
+import json
+import itertools
 
 __all__ = ['BIDSLayout']
+
+escape_dict = {'\a': r'\a',
+               '\b': r'\b',
+               '\c': r'\c',
+               '\f': r'\f',
+               '\n': r'\n',
+               '\r': r'\r',
+               '\t': r'\t',
+               '\v': r'\v',
+               '\'': r'\'',
+               '\"': r'\"',
+               '\0': r'\0',
+               '\1': r'\1',
+               '\2': r'\2',
+               '\3': r'\3',
+               '\4': r'\4',
+               '\5': r'\5',
+               '\6': r'\6',
+               '\7': r'\7',
+               '\8': r'\8',
+               '\9': r'\9'}
+
+
+def raw(text):
+    """Returns a raw string representation of text"""
+    new_string = ''
+    for char in text:
+        try:
+            new_string += escape_dict[char]
+        except KeyError:
+            new_string += char
+    return new_string
 
 
 class BIDSLayout(Layout):
@@ -11,6 +45,44 @@ class BIDSLayout(Layout):
             root = os.path.dirname(os.path.realpath(__file__))
             config = os.path.join(root, 'config', 'bids.json')
         super(BIDSLayout, self).__init__(path, config)
+
+    def get_metadata(self, path):
+        sidecarJSON = path.replace(".nii.gz", ".json").replace(".nii", ".json")
+        pathComponents = os.path.split(sidecarJSON)
+        filenameComponents = pathComponents[-1].split("_")
+        ses = None
+        suffix = filenameComponents[-1]
+
+        sub = filenameComponents[0]
+        keyword_components = filenameComponents[1:-1]
+        if filenameComponents[1][:3] == "ses":
+            ses = filenameComponents[1]
+            keyword_components = filenameComponents[2:-1]
+
+        potentialJSONs = []
+        for k in range(len(keyword_components)+1):
+            print(k)
+            for components in itertools.combinations(keyword_components, k):
+                print(components)
+                potentialJSONs.append(os.path.join(self.root, "_".join(components + (suffix,))))
+
+        for k in range(len(keyword_components)+1):
+            for components in itertools.combinations(keyword_components, k):
+                potentialJSONs.append(os.path.join(self.root, sub, "_".join((sub,) + components + (suffix,))))
+
+        if ses:
+            for k in range(len(keyword_components)+1):
+                for components in itertools.combinations(keyword_components, k):
+                    potentialJSONs.append(os.path.join(self.root, sub, ses, "_".join((sub, ses) + components + (suffix,))))
+
+        merged_param_dict = {}
+        for json_file_path in potentialJSONs:
+            print(json_file_path)
+            if os.path.exists(json_file_path):
+                param_dict = json.load(open(json_file_path, "r"))
+                merged_param_dict.update(param_dict)
+
+        return merged_param_dict
 
     def find_match(self, target, source=None):
 
@@ -28,8 +100,6 @@ class BIDSLayout(Layout):
             for root, directories, filenames in os.walk(self.root):
                 for f in filenames:
                     if re.search(target + '$', f):
-                        if os.path.sep == "\\":
-                            f = f.replace("\\", "\\\\")
                         candidates.append(f)
 
         if source is None:
@@ -48,10 +118,7 @@ class BIDSLayout(Layout):
         n_chunks = len(chunks)
         for i in range(n_chunks, -1, -1):
             path = os.path.join(self.root, *chunks[:i])
-            patt = path + '\%s[^\%s]+$' % (sep, sep)
-            if sep == "\\":
-                patt = path + '\\[^\\]+$'
-                patt = patt.replace("\\", "\\\\")
+            patt = raw(path) + r'\%s[^\%s]+$' % (sep, sep)
             matches = [x for x in candidates if re.search(patt, x)]
             if matches:
                 if len(matches) == 1:
--- bids/grabbids/tests/test_grabbids.py
+++ bids/grabbids/tests/test_grabbids.py
@@ -11,12 +11,10 @@ def test_layout_init():
     assert isinstance(layout.files, dict)
 
 
-def test_find_nearest():
+def test_gwt_metadata():
     data_dir = join(dirname(__file__), 'data', '7t_trt')
     layout = BIDSLayout(data_dir)
     target = 'sub-03/ses-2/func/sub-03_ses-2_task-' \
              'rest_acq-fullbrain_run-2_bold.nii.gz'
-    result = layout.find_match('bval', join(data_dir, *target.split('/')))
-    # TODO: this test does not make much sense
-    # TODO: there are no bval files for _bold
-    assert 'sub-03-test.bval' in result
+    result = layout.get_metadata(join(data_dir, target))
+    assert result['RepetitionTime'] == 3.0
