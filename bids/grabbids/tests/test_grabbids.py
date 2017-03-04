d082ae97b9d32cc5b0555770a23e81ab7072a9df
Yaroslav Halchenko <debian@onerussian.com>
2017-03-04 18:03:12 -0500

    RF: reduce duplication through a little fixture for BIDSLayout

--- bids/grabbids/tests/test_grabbids.py
+++ bids/grabbids/tests/test_grabbids.py
@@ -1,56 +1,51 @@
-from bids.grabbids import BIDSLayout
-from os.path import join, dirname
-
 """ Tests of BIDS-specific functionality. Generic tests of core grabbit
 functionality should go in the grabbit package. """
 
+import pytest
+from bids.grabbids import BIDSLayout
+from os.path import join, dirname
 
-def test_layout_init():
+# Fixture uses in the rest of the tests
+@pytest.fixture
+def testlayout1():
     data_dir = join(dirname(__file__), 'data', '7t_trt')
-    layout = BIDSLayout(data_dir)
-    assert isinstance(layout.files, dict)
+    return BIDSLayout(data_dir)
 
 
-def test_get_metadata():
-    data_dir = join(dirname(__file__), 'data', '7t_trt')
-    layout = BIDSLayout(data_dir)
+def test_layout_init(testlayout1):
+    assert isinstance(testlayout1.files, dict)
+
+
+def test_get_metadata(testlayout1):
     target = 'sub-03/ses-2/func/sub-03_ses-2_task-' \
              'rest_acq-fullbrain_run-2_bold.nii.gz'
-    result = layout.get_metadata(join(data_dir, target))
+    result = testlayout1.get_metadata(join(testlayout1.root, target))
     assert result['RepetitionTime'] == 3.0
 
 
-def test_get_metadata2():
-    data_dir = join(dirname(__file__), 'data', '7t_trt')
-    layout = BIDSLayout(data_dir)
+def test_get_metadata2(testlayout1):
     target = 'sub-03/ses-2/fmap/sub-03_ses-1_run-1_phasediff.nii.gz'
-    result = layout.get_metadata(join(data_dir, target))
+    result = testlayout1.get_metadata(join(testlayout1.root, target))
     assert result['EchoTime1'] == 0.006
 
 
-def test_get_subjects():
-    data_dir = join(dirname(__file__), 'data', '7t_trt')
-    layout = BIDSLayout(data_dir)
-    result = layout.get_subjects()
+def test_get_subjects(testlayout1):
+    result = testlayout1.get_subjects()
     predicted = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
     assert predicted == result
 
 
-def test_get_fieldmap():
-    data_dir = join(dirname(__file__), 'data', '7t_trt')
-    layout = BIDSLayout(data_dir)
+def test_get_fieldmap(testlayout1):
     target = 'sub-03/ses-1/func/sub-03_ses-1_task-' \
              'rest_acq-fullbrain_run-1_bold.nii.gz'
-    result = layout.get_fieldmap(join(data_dir, target))
+    result = testlayout1.get_fieldmap(join(testlayout1.root, target))
     assert result["type"] == "phasediff"
     assert result["phasediff"].endswith('sub-03_ses-1_run-1_phasediff.nii.gz')
 
 
-def test_get_fieldmap2():
-    data_dir = join(dirname(__file__), 'data', '7t_trt')
-    layout = BIDSLayout(data_dir)
+def test_get_fieldmap2(testlayout1):
     target = 'sub-03/ses-2/func/sub-03_ses-2_task-' \
              'rest_acq-fullbrain_run-2_bold.nii.gz'
-    result = layout.get_fieldmap(join(data_dir, target))
+    result = testlayout1.get_fieldmap(join(testlayout1.root, target))
     assert result["type"] == "phasediff"
     assert result["phasediff"].endswith('sub-03_ses-2_run-2_phasediff.nii.gz')
