9db0468fb444b495866952c31a778b9b3ac9997d
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2017-03-14 15:44:35 -0700

    fix line legth

--- bids/grabbids/bids_layout.py
+++ bids/grabbids/bids_layout.py
@@ -18,7 +18,8 @@ class BIDSLayout(Layout):
         if config is None:
             root = dirname(realpath(__file__))
             config = pathjoin(root, 'config', 'bids.json')
-        super(BIDSLayout, self).__init__(path, config, dynamic_getters=True, **kwargs)
+        super(BIDSLayout, self).__init__(path, config, 
+                                         dynamic_getters=True, **kwargs)
 
     def get_metadata(self, path):
         sidecarJSON = path.replace(".nii.gz", ".json").replace(".nii", ".json")
