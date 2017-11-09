37e39d5d5b6cd68bb88d0464f4d302ee227e2167
Christopher J. Markiewicz <markiewicz@stanford.edu>
2017-11-09 14:26:52 -0800

    design -> analysis

--- bids/__init__.py
+++ bids/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 from .version import __version__  # noqa
 from .due import due, Doi
-__all__ = ["grabbids", "design"]
+__all__ = ["grabbids", "analysis"]
 
 due.cite(Doi("10.1038/sdata.2016.44"),
          description="Brain Imaging Data Structure",
