b55bd2ac4fc21056f989cfee74450b6d9d035f46
Dylan <Dylan.Nielson@nih.gov>
2018-02-01 12:48:54 -0500

    add auto_model

--- bids/analysis/__init__.py
+++ bids/analysis/__init__.py
@@ -1,4 +1,6 @@
 from bids.variables import load_variables
 from .analysis import Analysis
+from .auto_model import auto_model
 
-__all__ = ['load_variables', 'Analysis']
+__all__ = ['load_variables', 'Analysis', 'auto_model']
+
