4981ac7f44c9b840b85c3b7cee55258007d1af50
Tal Yarkoni <tyarkoni@gmail.com>
2018-04-25 19:29:47 -0500

    reorder transformations alphabetically

--- bids/analysis/transformations/__init__.py
+++ bids/analysis/transformations/__init__.py
@@ -2,18 +2,18 @@ from .compute import (sum, product, scale, orthogonalize, threshold, and_, or_)
 from .munge import (split, rename, assign, copy, factor, filter, select)
 
 __all__ = [
-    'sum',
-    'product',
     'and_',
-    'or_',
-    'scale',
-    'orthogonalize',
-    'threshold',
-    'split',
-    'rename',
     'assign',
     'copy',
     'factor',
     'filter',
-    'select'
+    'or_',
+    'orthogonalize',
+    'product',
+    'rename',
+    'scale',
+    'select',
+    'split',
+    'sum',
+    'threshold'
 ]
