8d758c5141761f56e3ea17e4a4cdc6edfa9de7e3
adelavega <delavega@utexas.edu>
2018-07-27 17:36:11 -0500

    Avoid NaN in pivot_table

--- bids/variables/kollekshuns.py
+++ bids/variables/kollekshuns.py
@@ -97,7 +97,7 @@ class BIDSVariableCollection(object):
 
         Returns: A pandas DataFrame.
         '''
-        
+
         if variables is None:
             variables = list(self.variables.keys())
 
@@ -113,9 +113,11 @@ class BIDSVariableCollection(object):
             return df.reset_index(drop=True).fillna(fillna)
 
         ind_cols = list(set(df.columns) - {'condition', 'amplitude'})
+
+        df['amplitude'] = df['amplitude'].fillna('n/a')
         df = df.pivot_table(index=ind_cols, columns='condition',
-                            values='amplitude', aggfunc='first', dropna=False)
-        df = df.reset_index().fillna(fillna)
+                            values='amplitude', aggfunc='first')
+        df = df.reset_index().replace('n/a', fillna)
         df.columns.name = None
         return df
 
