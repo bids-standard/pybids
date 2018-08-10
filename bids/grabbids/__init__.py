d32fda469ea469fffafb221abc502e4156601cce
Lee Tirrell <ltirrell@corticometrics.com>
2018-08-10 11:02:08 -0700

    update version for removing grabbids

--- bids/grabbids/__init__.py
+++ bids/grabbids/__init__.py
@@ -3,4 +3,4 @@ from ..layout.bids_layout import BIDSLayout
 from ..layout.bids_validator import BIDSValidator
 __all__ = ["BIDSLayout", "BIDSValidator"]
 
-warnings.warn("grabbids has been renamed to layout in version 0.6.5, and will be removed in version 0.7", FutureWarning)
+warnings.warn("grabbids has been renamed to layout in version 0.6.5, and will be removed in version 0.8", FutureWarning)
