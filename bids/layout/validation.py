8e158a11af9b1870910d9d7ff12926eff903be99
Tal Yarkoni <tyarkoni@gmail.com>
2018-09-11 12:50:52 -0400

    temporarily store validation rules internally

--- bids/layout/validation.py
+++ bids/layout/validation.py
@@ -1,4 +1,8 @@
+"""Tools for validating BIDS projects."""
+
 import re
+import json
+from os.path import join, abspath, dirname
 
 __all__ = ['BIDSValidator']
 
@@ -18,7 +22,7 @@ class BIDSValidator():
 
     Examples
     --------
-    >>> from bids.layout import BIDSValidator
+    >>> from bids.grabbids import BIDSValidator
     >>> validator = BIDSValidator()
     >>> filepaths = ["/sub-01/anat/sub-01_rec-CSD_T1w.nii.gz",
     >>> "/sub-01/anat/sub-01_acq-23_rec-CSD_T1w.exe", #wrong extension
@@ -31,14 +35,12 @@ class BIDSValidator():
     """
 
     def __init__(self, index_associated=True):
-        self.anat_suffixes = ["T1w", "T2w", "T1map", "T2map",
-                              "T1rho", "FLAIR", "PD", "PDT2",
-                              "inplaneT1", "inplaneT2", "angio",
-                              "defacemask", "SWImagandphase"]
+        self.rule_dir = join(dirname(abspath(__file__)), 'config',
+                              'validator')
         self.index_associated = index_associated
 
     def is_bids(self, path):
-        """Checks if a file path appropriate for BIDS.
+        """Check if a file path appropriate for BIDS.
 
         Main method of the validator. uses other class methods for checking
         different aspects of the file path.
@@ -50,205 +52,132 @@ class BIDSValidator():
 
         Examples
         --------
-        >>> from bids.layout import BIDSValidator
+        >>> from bids.grabbids import BIDSValidator
         >>> validator = BIDSValidator()
         >>> validator.is_bids("/sub-01/ses-test/anat/sub-01_ses-test_rec-CSD_run-23_T1w.nii.gz")
         True
         >>> validator.is_bids("/sub-01/ses-test/sub-01_run-01_dwi.bvec") # missed session in the filename
         False
         """
+        conditions = []
 
-        return (
-            self.is_top_level(path) |
-            self.is_associated_data(path) |
-            self.is_session_level(path) |
-            self.is_subject_level(path) |
-            self.is_anat(path) |
-            self.is_dwi(path) |
-            self.is_func(path) |
-            self.is_behavioral(path) |
-            self.is_cont(path) |
-            self.is_field_map(path) |
-            self.is_phenotypic(path)
-        )
+        conditions.append(self.is_top_level(path))
+        conditions.append(self.is_associated_data(path))
+        conditions.append(self.is_session_level(path))
+        conditions.append(self.is_subject_level(path))
+        conditions.append(self.is_phenotypic(path))
+        conditions.append(self.is_file(path))
+
+        return (any(conditions))
 
     def is_top_level(self, path):
-        ''' Check if the file has appropriate name for a top-level file. '''
-        fixed_top_level_names = ["/README", "/CHANGES", "/dataset_description.json", "/participants.tsv",
-                                 "/participants.json", "/phasediff.json", "/phase1.json", "/phase2.json", "/fieldmap.json"]
+        """Check if the file has appropriate name for a top-level file."""
+        with open(join(self.rule_dir, 'fixed_top_level_names.json'), 'r') as f:
+            fixed_top_level_json = json.load(f)
+            fixed_top_level_names = fixed_top_level_json['fixed_top_level_names']
 
-        func_top_re = re.compile('^\\/(?:ses-[a-zA-Z0-9]+_)?(?:recording-[a-zA-Z0-9]+_)?task-[a-zA-Z0-9]+(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?(?:_echo-[0-9]+)?'
-                                 + '(_bold.json|_sbref.json|_events.json|_events.tsv|_physio.json|_stim.json|_beh.json)$')
-        func_top_flag = False if func_top_re.search(path) is None else True
+        regexps = self.get_regular_expressions('top_level_rules.json')
 
-        anat_top_re = re.compile('^\\/(?:ses-[a-zA-Z0-9]+_)?(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+_)?'
-                                 + '(' + "|".join(self.anat_suffixes) + ').json$')
-        anat_top_flag = False if anat_top_re.search(path) is None else True
+        conditions = [False if re.compile(x).search(path) is None else True
+                      for x in regexps]
 
-        dwi_top_re = re.compile('^\\/(?:ses-[a-zA-Z0-9]+)?(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?(?:_)?'
-                                + 'dwi.(?:json|bval|bvec)$')
-        dwi_top_flag = False if dwi_top_re.search(path) is None else True
+        conditions.append(path in fixed_top_level_names)
 
-        multi_dir_fieldmap_re = re.compile(
-            '^\\/(?:dir-[a-zA-Z0-9]+)_epi.json$')
-        multi_dir_fieldmap_flag = False if multi_dir_fieldmap_re.search(
-            path) is None else True
-
-        other_top_files_re = re.compile('^\\/(?:ses-[a-zA-Z0-9]+_)?(?:recording-[a-zA-Z0-9]+)?(?:task-[a-zA-Z0-9]+_)?(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?'
-                                        + '(_physio.json|_stim.json)$')
-        other_top_files_flag = False if other_top_files_re.search(
-            path) is None else True
-
-        check_index = path in fixed_top_level_names
-        return (check_index |
-                func_top_flag | anat_top_flag |
-                dwi_top_flag | multi_dir_fieldmap_flag | other_top_files_flag)
+        return (any(conditions))
 
     def is_associated_data(self, path):
-        ''' Check if file is appropriate associated data. '''
+        """Check if file is appropriate associated data."""
         if not self.index_associated:
             return False
-        associated_data_re = re.compile(
-            '^\\/(?:code|derivatives|sourcedata|stimuli)\\/(?:.*)$')
-        associated_data_flag = associated_data_re.search(path)
-        return associated_data_flag is not None
 
-    def is_phenotypic(self, path):
-        ''' Check if file is phenotypic data. '''
-        phenotypic_data = re.compile('^\\/(?:phenotype)\\/(?:.*.tsv|.*.json)$')
-        return phenotypic_data.search(path) is not None
+        regexps = self.get_regular_expressions('associated_data_rules.json')
+
+        conditions = [(re.compile(x).search(path) is not None)
+                      for x in regexps]
+
+        return any(conditions)
 
     def is_session_level(self, path):
-        ''' Check if the file has appropriate name for a session level. '''
-        scans_re = re.compile('^\\/(sub-[a-zA-Z0-9]+)' +
-                              '\\/(?:(ses-[a-zA-Z0-9]+)' +
-                              '\\/)?\\1(_\\2)?(_scans.tsv|_scans.json)$')
+        """Check if the file has appropriate name for a session level."""
+        regexps = self.get_regular_expressions('session_level_rules.json')
 
-        func_ses_re = re.compile('^\\/(sub-[a-zA-Z0-9]+)' +
-                                 '\\/(?:(ses-[a-zA-Z0-9]+)' +
-                                 '\\/)?\\1(_\\2)?task-[a-zA-Z0-9]+(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?(?:_echo-[0-9]+)?'
-                                 + '(_bold.json|_sbref.json|_events.json|_events.tsv|_physio.json|_stim.json)$')
+        conditions = [self.conditional_match(x, path) for x in regexps]
 
-        anat_ses_re = re.compile('^\\/(sub-[a-zA-Z0-9]+)' +
-                                 '\\/(?:(ses-[a-zA-Z0-9]+)' +
-                                 '\\/)?\\1(_\\2)?(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+_)?'
-                                 + '(' + ("|").join(self.anat_suffixes) + ').json$')
-
-        dwi_ses_re = re.compile('^\\/(sub-[a-zA-Z0-9]+)' +
-                                '\\/(?:(ses-[a-zA-Z0-9]+)' +
-                                '\\/)?\\1(_\\2)?(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?(?:_)?'
-                                + 'dwi.(?:json|bval|bvec)$')
-
-        return (self.conditional_match(scans_re, path) |
-                self.conditional_match(func_ses_re, path) |
-                self.conditional_match(anat_ses_re, path) |
-                self.conditional_match(dwi_ses_re, path))
+        return (any(conditions))
 
     def is_subject_level(self, path):
-        ''' Check if the file has appropriate name for a subject level. '''
-        scans_re = re.compile('^\\/(sub-[a-zA-Z0-9]+)' +
-                              '\\/\\1(_sessions.tsv|_sessions.json)$')
-        return scans_re.search(path) is not None
+        """Check if the file has appropriate name for a subject level."""
+        regexps = self.get_regular_expressions('subject_level_rules.json')
 
-    def is_anat(self, path):
-        ''' Check if the file has a name appropriate for an anatomical scan.
-        '''
-        anat_re = re.compile('^\\/(sub-[a-zA-Z0-9]+)' +
-                             '\\/(?:(ses-[a-zA-Z0-9]+)' +
-                             '\\/)?anat' +
-                             '\\/\\1(_\\2)?(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?_(?:'
-                             + "|".join(self.anat_suffixes)
-                             + ').(nii.gz|nii|json)$')
-        return self.conditional_match(anat_re, path)
+        conditions = [(re.compile(x).search(path) is not None)
+                      for x in regexps]
 
-    def is_dwi(self, path):
-        ''' Check if the file has a name appropriate for a diffusion scan. '''
-        suffixes = ["dwi", "sbref"]
-        anat_re = re.compile('^\\/(sub-[a-zA-Z0-9]+)' +
-                             '\\/(?:(ses-[a-zA-Z0-9]+)' +
-                             '\\/)?dwi' +
-                             '\\/\\1(_\\2)?(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?_(?:'
-                             + ("|").join(suffixes)
-                             + ').(nii.gz|nii|json|bvec|bval)$')
-        return self.conditional_match(anat_re, path)
+        return (any(conditions))
 
-    def is_field_map(self, path):
-        ''' Check if the file has a name appropriate for a fieldmap scan. '''
-        suffixes = ["phasediff", "phase1", "phase2", "magnitude1",
-                    "magnitude2", "magnitude", "fieldmap", "epi"]
-        anat_re = re.compile('^\\/(sub-[a-zA-Z0-9]+)' +
-                             '\\/(?:(ses-[a-zA-Z0-9]+)' +
-                             '\\/)?fmap' +
-                             '\\/\\1(_\\2)?(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_dir-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?_(?:'
-                             + ("|").join(suffixes)
-                             + ').(nii.gz|nii|json)$')
-        return self.conditional_match(anat_re, path)
+    def is_phenotypic(self, path):
+        """Check if file is phenotypic data."""
+        regexps = self.get_regular_expressions('phenotypic_rules.json')
 
-    def is_func(self, path):
-        ''' Check if the file has a name appropriate for a functional scan. '''
-        func_re = re.compile('^\\/(sub-[a-zA-Z0-9]+)' +
-                             '\\/(?:(ses-[a-zA-Z0-9]+)' +
-                             '\\/)?func' +
-                             '\\/\\1(_\\2)?_task-[a-zA-Z0-9]+(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?(?:_echo-[0-9]+)?'
-                             + '(?:_bold.nii.gz|_bold.nii|_bold.json|_sbref.nii.gz|_sbref.json|_events.json|_events.tsv|_physio.tsv.gz|_stim.tsv.gz|_physio.json|_stim.json|_defacemask.nii.gz|_defacemask.nii)$')
-        return self.conditional_match(func_re, path)
+        conditions = [(re.compile(x).search(path) is not None)
+                      for x in regexps]
 
-    def is_behavioral(self, path):
-        ''' Check if the file has a name appropriate for behavioral data. '''
-        func_beh = re.compile('^\\/(sub-[a-zA-Z0-9]+)' +
-                              '\\/(?:(ses-[a-zA-Z0-9]+)' +
-                              '\\/)?beh' +
-                              '\\/\\1(_\\2)?_task-[a-zA-Z0-9]+(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?'
-                              + '(?:_beh.json|_events.json|_events.tsv|_physio.tsv.gz|_stim.tsv.gz|_physio.json|_stim.json)$')
-        return self.conditional_match(func_beh, path)
+        return (any(conditions))
 
-    def is_func_bold(self, path):
-        ''' Check if the file has a name appropriate for functional bold. '''
-        func_re = re.compile('^\\/(sub-[a-zA-Z0-9]+)' +
-                             '\\/(?:(ses-[a-zA-Z0-9]+)' +
-                             '\\/)?func' +
-                             '\\/\\1(_\\2)?_task-[a-zA-Z0-9]+(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?' +
-                             '(?:_run-[0-9]+)?(?:_echo-[0-9]+)?'
-                             + '(?:_bold.nii.gz|_bold.nii|_sbref.nii.gz|_sbref.nii)$')
-        return self.conditional_match(func_re, path)
+    def is_file(self, path):
+        """Check if file is phenotypic data."""
+        regexps = self.get_regular_expressions('file_level_rules.json')
 
-    def is_cont(self, path):
-        ''' Check if the file has a name appropriate for physiological and
-        continuous recordings. '''
-        cont_re = re.compile('^\\/(sub-[a-zA-Z0-9]+)' +
-                             '\\/(?:(ses-[a-zA-Z0-9]+)' +
-                             '\\/)?(?:func|beh)' +
-                             '\\/\\1(_\\2)?_task-[a-zA-Z0-9]+(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?' +
-                             '(?:_recording-[a-zA-Z0-9]+)?'
-                             + '(?:_physio.tsv.gz|_stim.tsv.gz|_physio.json|_stim.json)$')
-        return self.conditional_match(cont_re, path)
+        conditions = [(re.compile(x).search(path) is not None)
+                      for x in regexps]
+
+        return (any(conditions))
+
+    def get_regular_expressions(self, filename):
+        """Get regular expressions from file."""
+        regexps = []
+
+        filename = join(self.rule_dir, filename)
+
+        with open(filename, 'r') as f:
+            rules = json.load(f)
+
+        for key in list(rules.keys()):
+            rule = rules[key]
+
+            regexp = rule["regexp"]
+
+            if "tokens" in rule:
+                tokens = rule["tokens"]
+
+                for token in list(tokens):
+                    regexp = regexp.replace(token, "|".join(tokens[token]))
+
+            regexps.append(regexp)
+
+        return regexps
 
     def get_path_values(self, path):
-        ''' Takes a file path and returns values found for the following path
+        """Takes a file path and returns values found for the following path
         keys:
             sub-
             ses-
-        '''
+        """
         values = {}
-        # capture subject
-        match = re.compile(r'/^\/sub-([a-zA-Z0-9]+)/)').findall(path)
-        values['sub'] = match[1] if match & match[1] else None
 
-        # capture session
-        match = re.compile(
-            r'/^\/sub-[a-zA-Z0-9]+\/ses-([a-zA-Z0-9]+)/').findall(path)
-        values['ses'] = match[1] if match & match[1] else None
+        regexps = self.get_regular_expressions('path.json')
+
+        # capture subject
+        for paths in ['sub', 'ses']:
+            match = re.compile(regexps[paths]).findall(path)
+            values[paths] = match[1] if match & match[1] else None
 
         return values
 
     def conditional_match(self, expression, path):
-        match = expression.findall(path)
+        match = re.compile(expression).findall(path)
         match = match[0] if len(match) >= 1 else False
         # adapted from JS code and JS does not support conditional groups
         if (match):
-            if ((match[1] == match[2][1:]) | (not match[1])):
-                return True
-            else:
-                return False
+            return ((match[1] == match[2][1:]) | (not match[1]))
         else:
             return False
