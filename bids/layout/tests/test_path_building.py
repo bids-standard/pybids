2e7e83a0c3b789a0d0ba89134b64a0f6b723c3af
Tal Yarkoni <tyarkoni@gmail.com>
2018-09-14 11:06:42 -0400

    add forgotten path-building test

---
+++ bids/layout/tests/test_path_building.py
@@ -0,0 +1,16 @@
+import pytest
+from bids.layout import BIDSLayout
+from os.path import join, abspath, sep
+from bids.tests import get_test_data_path
+
+
+@pytest.fixture(scope='module')
+def layout():
+    data_dir = join(get_test_data_path(), '7t_trt')
+    return BIDSLayout(data_dir)
+
+def test_bold_construction(layout):
+    ents = dict(subject='01', run=1, task='rest', suffix='bold')
+    assert layout.build_path(ents) == "sub-01/func/sub-01_task-rest_run-1_bold.nii.gz"
+    ents['acquisition'] = 'random'
+    assert layout.build_path(ents) == "sub-01/func/sub-01_task-rest_acq-random_run-1_bold.nii.gz"
