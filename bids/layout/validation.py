cb20fe36afe14460156f28d02d110bb355bca71c
adelavega <delavega@utexas.edu>
2018-10-09 16:29:04 -0400

    update rule dir

--- bids/layout/validation.py
+++ bids/layout/validation.py
@@ -35,8 +35,7 @@ class BIDSValidator():
     """
 
     def __init__(self, index_associated=True):
-        self.rule_dir = join(dirname(abspath(__file__)), 'config',
-                              'validator')
+        self.rule_dir = join(dirname(abspath(__file__)),'config')
         self.index_associated = index_associated
 
     def is_bids(self, path):
