f71a166598ab35bc15f298226bb510d43f78c810
adelavega <delavega@utexas.edu>
2018-11-02 18:49:45 -0500

    Fix imports for renamed transformations

--- bids/analysis/transformations/__init__.py
+++ bids/analysis/transformations/__init__.py
@@ -1,21 +1,21 @@
 from .compute import (sum, product, scale, orthogonalize, threshold, and_, or_,
-                      not_, demean, convolve_HRF)
+                      not_, demean, convolve)
 from .munge import (split, rename, assign, copy, factor, filter, select,
-                    remove, replace, to_dense)
+                    delete, replace, to_dense)
 
 __all__ = [
     'and_',
     'assign',
-    'convolve_HRF',
+    'convolve',
     'copy',
     'demean',
+    'delete',
     'factor',
     'filter',
     'not_',
     'or_',
     'orthogonalize',
     'product',
-    'remove',
     'rename',
     'replace',
     'scale',
