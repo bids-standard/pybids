93973b541b218888f26cbd048c930a8b3df84294
TheJoggler <johanneswennberg@gmail.com>
2019-02-27 15:01:55 +0100

    Expanded note about changed behaviour of get()

--- bids/layout/layout.py
+++ bids/layout/layout.py
@@ -465,6 +465,13 @@ class BIDSLayout(object):
 
         Returns:
             A list of BIDSFiles (default) or strings (see return_type).
+
+        Notes:
+            As of pybids 0.7.0 some keywords have been changed. Namely: 'type'
+            becomes 'suffix', 'modality' becomes 'datatype', 'acq' is removed
+            and 'mod' becomes 'modality'. Using the wrong version could result
+            in the get() silently returning wrong or no results. See the
+            changelog for more details.
         """
 
         # Warn users still expecting 0.6 behavior
