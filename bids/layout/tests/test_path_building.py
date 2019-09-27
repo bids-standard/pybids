e3a19bdf09ce6e240a2b7d46291d8aa3d0997eee
Steven Tilley <steve@steventilley.com>
2019-09-27 10:40:20 -0400

    add test for failed path building

--- bids/layout/tests/test_path_building.py
+++ bids/layout/tests/test_path_building.py
@@ -25,3 +25,9 @@ def test_invalid_file_construction(layout):
     
     target = "sub-01/func/sub-01_task-resting-state_run-1_bold.nii.gz"
     assert layout.build_path(ents, validate=False) == target
+
+
+def test_failed_file_construction(layout):
+    ents = dict(subject='01', fakekey='foobar')
+    with pytest.raises(ValueError):
+        layout.build_path(ents)
