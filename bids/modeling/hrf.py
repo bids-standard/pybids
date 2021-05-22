1310325c44b8df3860d89b5f8ce61910ef36e836
Christopher J. Markiewicz <markiewicz@stanford.edu>
2021-05-22 13:46:09 -0400

    FIX: Drop deprecated aliases np.int/float

--- bids/modeling/hrf.py
+++ bids/modeling/hrf.py
@@ -43,7 +43,7 @@ def _gamma_difference_hrf(tr, oversampling=50, time_length=32., onset=0.,
     """
     from scipy.stats import gamma
     dt = tr / oversampling
-    time_stamps = np.linspace(0, time_length, np.rint(float(time_length) / dt).astype(np.int))
+    time_stamps = np.linspace(0, time_length, np.rint(float(time_length) / dt).astype(int))
     time_stamps -= onset
     hrf = gamma.pdf(time_stamps, delay / dispersion, dt / dispersion) -\
         ratio * gamma.pdf(
@@ -239,7 +239,7 @@ def _sample_condition(exp_condition, frame_times, oversampling=50,
 
     hr_frame_times = np.linspace(frame_times.min() + min_onset,
                                  frame_times.max() * (1 + 1. / (n - 1)),
-                                 np.rint(n_hr).astype(np.int))
+                                 np.rint(n_hr).astype(int))
 
     # Get the condition information
     onsets, durations, values = tuple(map(np.asanyarray, exp_condition))
@@ -250,7 +250,7 @@ def _sample_condition(exp_condition, frame_times, oversampling=50,
 
     # Set up the regressor timecourse
     tmax = len(hr_frame_times)
-    regressor = np.zeros_like(hr_frame_times).astype(np.float)
+    regressor = np.zeros_like(hr_frame_times).astype(float)
     t_onset = np.minimum(np.searchsorted(hr_frame_times, onsets), tmax - 1)
     regressor[t_onset] += values
     t_offset = np.minimum(
