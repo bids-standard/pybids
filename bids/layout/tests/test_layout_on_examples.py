196cb0f07454f0ad9c294f17f8b042306e203e1a
Remi Gau <remi_gau@hotmail.com>
2022-04-18 21:05:55 +0200

    flag qmri_qsm as failure expected too

--- bids/layout/tests/test_layout_on_examples.py
+++ bids/layout/tests/test_layout_on_examples.py
@@ -20,7 +20,6 @@ from bids.tests import get_test_data_path
         ("qmri_mp2rage", 14),
         ("qmri_mp2rageme", 28),
         ("qmri_mtsat", 23),
-        ("qmri_qsm", 8),
         ("qmri_sa2rage", 9),
         ("qmri_vfa", 17),
     ],
@@ -58,15 +57,16 @@ def test_layout_on_examples_no_derivatives(dataset, nb_files):
     assert len(files) == nb_files
 
 
-@pytest.mark.xfail(reason="missing derivatives description: https://github.com/bids-standard/bids-examples/issues/310")
 @pytest.mark.parametrize(
     "dataset, nb_files",
     [
-        ("qmri_mpm", 125),
+        pytest.param("qmri_qsm", 8, marks=pytest.mark.xfail(reason="https://github.com/bids-standard/bids-examples/issues/311")),
+        pytest.param("qmri_mpm", 125, marks=pytest.mark.xfail(reason="https://github.com/bids-standard/bids-examples/issues/310")),
     ],
 )
-def test_layout_on_examples_with_derivatives(dataset, nb_files):
+def test_layout_on_examples_invalid_ds_description(dataset, nb_files):
     ds = join(get_test_data_path(), "bids-examples", dataset)
     layout = BIDSLayout(ds, derivatives=True)
     files = layout.get()
     assert len(files) == nb_files
+
