bf959178e8fb37876845dcb7dc38eb627cddeec0
Remi Gau <remi_gau@hotmail.com>
2022-04-19 08:11:56 +0200

    make xfail strict to detect tests when they start passing

--- bids/layout/tests/test_layout_on_examples.py
+++ bids/layout/tests/test_layout_on_examples.py
@@ -1,5 +1,10 @@
 """ Tests runs layout on bids examples and make sure all files are caught"""
 
+""" TODO
+- add more 'vanilla' datasets
+- missing files in micr?
+"""
+
 from os.path import join
 
 import pytest
@@ -11,6 +16,7 @@ from bids.tests import get_test_data_path
 #
 #   find ds_name -type f | wc -l
 #
+
 
 @pytest.mark.parametrize(
     "dataset, nb_files",
@@ -60,8 +66,20 @@ def test_layout_on_examples_no_derivatives(dataset, nb_files):
 @pytest.mark.parametrize(
     "dataset, nb_files",
     [
-        pytest.param("qmri_qsm", 8, marks=pytest.mark.xfail(reason="https://github.com/bids-standard/bids-examples/issues/311")),
-        pytest.param("qmri_mpm", 125, marks=pytest.mark.xfail(reason="https://github.com/bids-standard/bids-examples/issues/310")),
+        pytest.param(
+            "qmri_qsm",
+            8,
+            marks=pytest.mark.xfail(strict=True,
+                reason="https://github.com/bids-standard/bids-examples/issues/311"
+            ),
+        ),
+        pytest.param(
+            "qmri_mpm",
+            125,
+            marks=pytest.mark.xfail(strict=True,
+                reason="https://github.com/bids-standard/bids-examples/issues/310"
+            ),
+        ),
     ],
 )
 def test_layout_on_examples_invalid_ds_description(dataset, nb_files):
@@ -69,4 +87,3 @@ def test_layout_on_examples_invalid_ds_description(dataset, nb_files):
     layout = BIDSLayout(ds, derivatives=True)
     files = layout.get()
     assert len(files) == nb_files
-
