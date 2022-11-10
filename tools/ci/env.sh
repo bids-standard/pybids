96648cdabd813512e97bfc0bf8f32eaa1b616cad
Chris Markiewicz <effigies@gmail.com>
2022-11-10 12:33:11 -0500

    CI: Remove deprecated legacy resolver

--- tools/ci/env.sh
+++ tools/ci/env.sh
@@ -4,8 +4,3 @@ SETUP_REQUIRES="pip build"
 NIGHTLY_WHEELS="https://pypi.anaconda.org/scipy-wheels-nightly/simple"
 STAGING_WHEELS="https://pypi.anaconda.org/multibuild-wheels-staging/simple"
 PRE_PIP_FLAGS="--pre --extra-index-url $NIGHTLY_WHEELS --extra-index-url $STAGING_WHEELS"
-
-# XXX This is necessary to deal with scipy version metadata mismatches
-# We should aim to remove this ASAP.
-# Last check: 2020.12.19
-PRE_PIP_FLAGS="--use-deprecated=legacy-resolver $PRE_PIP_FLAGS"
