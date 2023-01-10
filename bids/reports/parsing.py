84ba171fe7f739555e2343d86cabea44a9860807
Remi Gau <remi_gau@hotmail.com>
2023-01-10 13:31:27 +0100

    refactor

--- bids/reports/parsing.py
+++ bids/reports/parsing.py
@@ -120,18 +120,6 @@ def anat_info(files, config):
     fa_str = parameters.describe_flip_angle(metadata)
     fov_str, matrixsize_str, voxelsize_str = parameters.describe_image_size(img)
 
-    # parameters_str = [
-    #     slice_str,
-    #     f"repetition time, TR={tr}ms",
-    #     te_str,
-    #     fa_str,
-    #     fov_str,
-    #     matrixsize_str,
-    #     voxelsize_str,
-    # ]
-    # parameters_str = [d for d in parameters_str if len(d)]
-    # parameters_str = "; ".join(parameters_str)
-
     desc_data = {
         "scan_type": scan_type,
         "slice_str": slice_str, 
@@ -146,13 +134,11 @@ def anat_info(files, config):
         "seqs": seqs,
         "me_str": me_str}
 
-    # desc = f"""{nb_runs_str(nb_runs)} of {scan_type} {variants} {seqs} {me_str} structural MRI "
-    #     "data were collected ({parameters_str})."""
     return anat_info_template(desc_data)
 
 def anat_info_template(desc_data):
     desc = f"""{nb_runs_str(desc_data["nb_runs"])} of {desc_data["scan_type"]} {desc_data["variants"]} 
-{desc_data["seqs"]} {desc_data["me_str"]} structural MRI (repetition time, TR={desc_data["tr"]}ms; {desc_data["slice_str"]}; 
+{desc_data["seqs"]} {desc_data["me_str"]} structural MRI data were collected (repetition time, TR={desc_data["tr"]}ms; {desc_data["slice_str"]}; 
 {desc_data["te_str"]}; {desc_data["fa_str"]}; {desc_data["fov_str"]}; {desc_data["matrixsize_str"]}; 
 {desc_data["voxelsize_str"]})."""
 
@@ -193,24 +179,29 @@ def dwi_info(files, config):
     nvec_str = parameters.describe_dmri_directions(img)
     mb_str = parameters.describe_multiband_factor(metadata)
 
-    parameters_str = [
-        f"repetition time, TR={tr}ms",
-        te_str,
-        fa_str,
-        fov_str,
-        matrixsize_str,
-        voxelsize_str,
-        bval_str,
-        nvec_str,
-        mb_str,
-    ]
-    parameters_str = [d for d in parameters_str if len(d)]
-    parameters_str = "; ".join(parameters_str)
+    desc_data = {
+        "tr" : tr,
+        "te_str" : te_str,
+        "fa_str" : fa_str,
+        "fov_str" : fov_str,
+        "matrixsize_str" : matrixsize_str,
+        "voxelsize_str" : voxelsize_str,        
+        "nb_runs": nb_runs, 
+        "variants": variants,
+        "seqs": seqs,
+        "bval_str":        bval_str,
+        "nvec_str": nvec_str,
+        "mb_str": mb_str,}    
 
-    desc = f"""{nb_runs_str(nb_runs)} of {variants} {seqs} diffusion-weighted (dMRI) data were "
-        "collected ({parameters_str})."""
+    return dwi_info_template(desc_data)
+
+def dwi_info_template(desc_data):
+    desc = f"""{nb_runs_str(desc_data["nb_runs"])} of {desc_data["variants"]} 
+{desc_data["seqs"]} diffusion-weighted (dMRI) data were collected (repetition time, TR={desc_data["tr"]}ms; 
+{desc_data["bval_str"]}; {desc_data["nvec_str"]}; {desc_data["mb_str"]};
+{desc_data["te_str"]}; {desc_data["fa_str"]}; {desc_data["fov_str"]}; {desc_data["matrixsize_str"]}; 
+{desc_data["voxelsize_str"]})."""
     return desc
-
 
 def fmap_info(layout, files, config):
     """Generate a paragraph describing field map acquisition information.
