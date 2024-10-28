d96b7b4090e7a8d6cb38f33ad30e258ebf497e8d
Chris Markiewicz <effigies@gmail.com>
2024-10-28 11:22:47 -0400

    TEST: Use anonymous s3 connnection for test

--- bids/layout/tests/test_remote_bids.py
+++ bids/layout/tests/test_remote_bids.py
@@ -1,26 +1,24 @@
-""" Tests runs layout on bids examples and make sure all files are caught"""
+"""Tests runs layout on bids examples and make sure all files are caught"""
 
-""" TODO
-- add more 'vanilla' datasets
-- missing files in micr?
-"""
+# TODO
+# - add more 'vanilla' datasets
+# - missing files in micr?
 
 import pytest
+from upath import UPath
 
 from bids.layout import BIDSLayout
 
 # Values for the number of files by downloading dataset first
 
+
 @pytest.mark.parametrize(
     "dataset, nb_files",
     [
-        ("s3://openneuro.org/ds000102", 136),
+        (UPath("s3://openneuro.org/ds000102", anon=True), 136),
     ],
 )
 def test_layout_on_s3_datasets_no_derivatives(dataset, nb_files):
     layout = BIDSLayout(dataset)
     files = layout.get()
     assert len(files) == nb_files
-
-
-
