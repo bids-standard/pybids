962cfe7e678c067fa30215956380ba20a5f79b1c
Chris Markiewicz <effigies@gmail.com>
2024-11-04 11:07:40 -0500

    Update bids/layout/tests/test_remote_bids.py

--- bids/layout/tests/test_remote_bids.py
+++ bids/layout/tests/test_remote_bids.py
@@ -5,6 +5,7 @@
 # - missing files in micr?
 
 import pytest
+from botocore.exceptions import NoCredentialsError
 from upath import UPath
 
 from bids.layout import BIDSLayout
@@ -18,6 +19,7 @@ from bids.layout import BIDSLayout
         (UPath("s3://openneuro.org/ds000102", anon=True), 136),
     ],
 )
+@pytest.mark.xfail(raises=NoCredentialsError)
 def test_layout_on_s3_datasets_no_derivatives(dataset, nb_files):
     layout = BIDSLayout(dataset)
     files = layout.get()
